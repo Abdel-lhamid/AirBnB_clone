d45d5088d297821024988302f7b44a1ca4408376
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-03-23 16:48:17 -0700

    console do create done

--- console.py
+++ console.py
@@ -13,6 +13,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+    classes = {"BaseModel": BaseModel}
 
     def do_quit(self, arg):
         """Quit command, exits the consol"""
@@ -26,6 +27,29 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """empty line do nothing"""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of the BaseModel"""
+        if not arg:
+            print("** class name missing **")
+        args = arg.split()
+
+        if args[0] is not "BaseModel":
+            print("** class doesn't exist **")
+            return
+
+        new_inst = BaseModel()
+        new_inst.save()
+        print(new_inst.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance
+        based on the class name and id"""
+        if not arg:
+            print("** class name missing **")
+
+        args = args.split()
+        if args[0]
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
